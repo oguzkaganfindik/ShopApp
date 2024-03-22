af75c2d11eb4fe5c6bd4a1729e40a323075e4215
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-03-22 20:07:29 +0300

    DbSeeder Eklendi Ve View Partial Yapisi Genisletildi

--- ShopApp.Data/Context/ShopAppContext.cs
+++ ShopApp.Data/Context/ShopAppContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
 using ShopApp.Data.Entities;
 
 namespace ShopApp.Data.Context
 {
     public class ShopAppContext : DbContext
     {
-        public ShopAppContext(DbContextOptions<ShopAppContext> options) : base(options)
+        private readonly IDataProtector _dataProtector;
+        public ShopAppContext(DbContextOptions<ShopAppContext> options, IDataProtectionProvider dataProtectionProvider) : base(options)
         {
-            
+            _dataProtector = dataProtectionProvider.CreateProtector("security");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -16,6 +18,24 @@ namespace ShopApp.Data.Context
             modelBuilder.ApplyConfiguration(new UserConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            // Seed Data -> Veritabanı ilk ayaklandırıldığında gelecek olan veri.            
+            // var Password = "123";
+            // Password = _dataProtector.Protect(Password);
+
+            modelBuilder.Entity<UserEntity>().HasData(new List<UserEntity>()
+            {
+                new UserEntity() 
+                { 
+                    Id = 1,
+                    FirstName = "Şebnem",
+                    LastName = "Ferah",
+                    Email = "admin@test.com",
+                    Password = _dataProtector.Protect("123"),
+                    UserType = Enums.UserTypeEnum.Admin               
+                }
+            });
+
 
             base.OnModelCreating(modelBuilder);
         }
--- ShopApp.Data/Migrations/20240322052550_Second.Designer.cs
+++ ShopApp.Data/Migrations/20240322052550_Second.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopApp.Data.Context;
 
@@ -11,9 +12,11 @@ using ShopApp.Data.Context;
 namespace ShopApp.Data.Migrations
 {
     [DbContext(typeof(ShopAppContext))]
-    partial class ShopAppContextModelSnapshot : ModelSnapshot
+    [Migration("20240322052550_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
